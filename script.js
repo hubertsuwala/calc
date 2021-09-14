cd14b10fdc741a8b3fec71ebc0422a78967b4e22
hubertsuwala <hubert.suwala@yahoo.com>
2021-09-14 18:20:25 +0200

    a

--- script.js
+++ script.js
@@ -1,46 +1,88 @@
 'use strict';
+
+const previousNumber = document.querySelector('.previous-number');
 const score = document.querySelector('.score');
-const tap = document.querySelector('.tap');
-const clean = document.querySelector('.el-delete');
-const button = document.querySelectorAll('.button');
-const calculator = document.querySelector('.calculator');
+const mathSign = document.querySelector('.math-sign');
+const number = document.querySelectorAll('.number');
+const equal = document.querySelector('.el-equal');
+const clear = document.querySelector('.el-delete');
+const operator = document.querySelectorAll('.operator');
+// const comma = document.querySelectorAll('.el-comma');
 
-/////////////tap
-
-let currentTap;
-let start = 0;
-
-const init = function () {
-  currentTap = '';
-  console.log(currentTap);
-  tap.textContent = 0;
+let result = '';
+let nowy = '';
+const displayNumbers = function () {
+  if (this.textContent === '.' && score.textContent.includes('.')) return;
+  if (this.textContent === '.' && score.textContent === '')
+    return (score.textContent = '0.');
+  if (this.textContent === '0' && score.textContent === '0')
+    return (score.textContent = '0.');
+  if (result == '') return (score.textContent += this.textContent);
+  result = '0';
+  console.log(result);
+  console.log(score.textContent);
+  score.textContent += this.textContent;
+  //  ? (score.textContent += this.textContent) : (result = '');
 };
-init();
 
-calculator.addEventListener('click', function (e) {
-  const clicked = e.target.textContent;
-  console.log(tap.textContent);
-  // Matching strategy
-  if (
-    e.target.classList.contains('button')
-    // ||
-    // e.target.classList.contains('operation')
-  ) {
-    currentTap += clicked;
-    console.log(clicked);
-    tap.textContent = currentTap;
-    console.log(tap.textContent);
+const operate = function () {
+  if (score.textContent === '' && this.textContent === '') {
+    score.textContent = '-';
   }
-});
-clean.addEventListener('click', init);
+  if (score.textContent === '') return;
+  // score.textContent += result + this.textContent;
+  if (mathSign.textContent !== '') {
+    showResult();
+  }
+  previousNumber.textContent = score.textContent;
+  mathSign.textContent = this.textContent;
+  score.textContent = '';
+};
 
-// 2nd option
+const showResult = function () {
+  if (previousNumber.textContent === '' || score.textContent === '') return;
 
-// button.forEach(btn =>
-//   btn.addEventListener('click', function (e) {
-//     const clicked = e.target.textContent;
-//     console.log(clicked);
-//     tap.textContent = clicked;
-//   })
-// );
-// console.log(document.querySelector('.el-1').textContent);
+  switch (mathSign.textContent) {
+    case '+':
+      result = +score.textContent + +previousNumber.textContent;
+      break;
+    case '-':
+      result = +previousNumber.textContent - +score.textContent;
+      break;
+    case '*':
+      result = +score.textContent * +previousNumber.textContent;
+      break;
+    case '/':
+      score.textContent === '0'
+        ? (result = '')
+        : (result = +previousNumber.textContent / +score.textContent);
+      break;
+  }
+  // if (score.textContent === '0') return;
+  if (result === '0') return;
+
+  score.textContent = result;
+  previousNumber.textContent = '';
+  mathSign.textContent = '';
+  
+
+  // zrob kasowanie po nowym wprowadzeniu liczby po obliczeniu
+  // if (
+  //   result !== '0' &&
+  //   previousNumber.textContent === '' &&
+  //   mathSign.textContent === ''
+  // ) {
+  //   console.log('test');
+  // }
+};
+const clearAll = function () {
+  result = '';
+  score.textContent = '';
+  mathSign.textContent = '';
+  previousNumber.textContent = '';
+};
+
+number.forEach(btn => btn.addEventListener('click', displayNumbers));
+operator.forEach(btn => btn.addEventListener('click', operate));
+equal.addEventListener('click', showResult);
+clear.addEventListener('click', clearAll);
